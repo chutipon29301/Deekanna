66f9982ea6bb13619e1ecf09dfdcb3d3c5995d40
WhatTheFar <ichannel.jakpat@gmail.com>
2016-10-20 13:21:09 +0700

    fix option

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,5 @@
-var slideIndex = 1;
-			showDivs(slideIndex);
-
-			function plusDivs(n) {
-			    showDivs(slideIndex += n);
-			}
-
-			function showDivs(n) {
-			    var i;
-			    var x = document.getElementsByClassName("mySlides");
-			    if (n > x.length) {slideIndex = 1} 
-			    if (n < 1) {slideIndex = x.length} ;
-			    for (i = 0; i < x.length; i++) {
-			        x[i].style.display = "none";
-			    }
-			    x[slideIndex-1].style.display = "block"; 
-			}
-
 $(document).ready(function() {
-     $('.toggle-nav').click(function() {
+    $('.toggle-nav').click(function() {
          $('.panel').slideToggle('slow')
     });
 });
